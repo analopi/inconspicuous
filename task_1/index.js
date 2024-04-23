771a818c29d769c72656d496035265596dc122fa
Vitaliy Alekseevich <vit.k2000@yandex.ru>
2024-04-24 01:44:16 +0500

    Тестирование

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,36 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
 
 
+function decodeFields(encodedData, translationsDict) {
 
-// console.log(decoded)
+    const uniqueIds = new Set();
+    
+    const decodedData = encodedData.map(obj => {
+      const decodedObj = {};
+  
+
+      for (const key in obj) {
+        if (key.endsWith("Id") && !["groupId", "service", "formatSize", "ca"].includes(key)) {
+          const decodedValue = translationsDict[obj[key]] || obj[key]; 
+          decodedObj[key] = decodedValue;
+          uniqueIds.add(obj[key]); 
+        } else {
+
+          decodedObj[key] = obj[key];
+        }
+      }
+  
+      return decodedObj;
+    });
+  
+
+    return { decodedData, uniqueIds: Array.from(uniqueIds) };
+  }
+  
+
+const { decodedData, uniqueIds } = decodeFields(encoded, translations);
+console.log("Decoded Data:", decodedData);
+console.log("Unique IDs:", uniqueIds);
+
+
--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,80 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import { onMount } from 'svelte';
+
+  let fromCurrency = '';
+  let toCurrency = '';
+  let amount = '';
+  let exchangeRate = null;
+  let currencyList = [];
+
+  function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+      clearTimeout(timeoutId);
+      console.log(func);
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  }
+
+  async function fetchExchangeRate() {
+    if (fromCurrency && toCurrency && amount) {
+      const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`);
+      const data = await response.json();
+      exchangeRate = data.rates[toCurrency];
+    }
+  }
+
+  async function fetchCurrencyList() {
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/RUB`);
+    const data = await response.json();
+    currencyList = Object.keys(data.rates);
+  }
+
+  const debouncedFetchExchangeRate = debounce(fetchExchangeRate, 300);
+
+  function handleFromCurrencyChange(event) {
+    fromCurrency = event.target.value.toUpperCase();
+    debouncedFetchExchangeRate();
+  }
+
+  function handleToCurrencyChange(event) {
+    toCurrency = event.target.value.toUpperCase();
+    debouncedFetchExchangeRate();
+  }
+
+  function handleAmountInput(event) {
+    amount = event.target.value;
+    debouncedFetchExchangeRate();
+  }
+
+  onMount(fetchExchangeRate);
+  onMount(fetchCurrencyList);
+
+  $: convertedAmount = (amount * exchangeRate).toFixed(2);
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+<div>
+  <label>Ваша валюта:</label>
+  <select bind:value={fromCurrency} on:change={handleFromCurrencyChange}>
+    {#each currencyList as currency}
+      <option value={currency}>{currency}</option>
+    {/each}
+  </select>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+  <label>Переводим в:</label>
+  <select bind:value={toCurrency} on:change={handleToCurrencyChange}>
+    {#each currencyList as currency}
+      <option value={currency}>{currency}</option>
+    {/each}
+  </select>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  <label>Кол-во в {fromCurrency}</label>
+  <input type="text" bind:value={amount} on:input={handleAmountInput}>
+
+  <label>Кол-во в {toCurrency}</label>
+  <input type="text" readonly bind:value={convertedAmount}>
+</div>
